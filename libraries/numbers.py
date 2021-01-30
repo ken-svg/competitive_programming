eab54e208ceecdfdd11fcf26f809115980d6e6ba
ken-svg <76655271+ken-svg@users.noreply.github.com>
2021-01-30 17:29:29 +0900

    Update numbers.py

--- libraries/numbers.py
+++ libraries/numbers.py
@@ -8,3 +8,17 @@ def inv(x):
   if x not in _inv_t:
     _inv_t[x] = inv(mod % x) * (mod - mod // x) % mod
   return _inv_t[x]
+
+# floor_sum
+import sys
+sys.setrecursionlimit(10**6)
+def floor_sum(n, m, a, b): # math.floor((a * i + b) / m) の 0 <= i <= N-1 の和を計算
+  if n == 0: return 0
+  ans = 0
+  ans += (n * (n-1) // 2) * (a // m)
+  ans += n * (b // m)
+  a %= m
+  b %= m
+  y = (a * (n-1) + b) // m
+  ans += y
+  return ans + floor_sum(y, a, m, (a * (n-1) + b) % m)
