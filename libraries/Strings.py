17b0ae46aed21fcaf18c5275a88d80abdbf44200
ken-svg <76655271+ken-svg@users.noreply.github.com>
2022-11-03 14:42:03 +0900

    Update Strings.py

--- libraries/Strings.py
+++ libraries/Strings.py
@@ -1,3 +1,4 @@
+# 10: Z_algorithm
 def Z_algorithm(S):
   # SとS[j:]の共通接頭辞長さを求める
   A = [0] * len(S)
@@ -24,7 +25,7 @@ def Z_algorithm(S):
   A[0] = -1
   return A
 
-
+# 20: MP_algorithm(prefix_function)
 def MP_algorithm(S):
   # S[:j]の接頭辞と接尾辞が一致するものの最長長さを求める（完全一致を除く）
   A = [0] * len(S)
@@ -38,3 +39,43 @@ def MP_algorithm(S):
       A[now] = step = 0
       
   return A
+
+# 20_1: _return_table (-> KMP_search)
+def _return_table(MPS, S):
+  # MPS(Sのprefix function)とSからKMPで使用する部分マッチテーブルを作成
+  W = [MPS[i]-1 for i in range(len(S))]
+  for i in range(1, len(S)-1):
+    s = S[i+1]
+    j = i
+    while j >= 0 and s == S[j+1]:
+      j = W[j]
+    W[i] = j
+  return W
+
+# 21: KMP_search
+def KMP_search(A, B, complete_search = False):
+  # 文字列AからBをサーチする
+  # MP_algorithmと_return_tableを必要とする
+  if len(A) < len(B):
+    return [] if complete_search else -1
+  
+  RTB = _return_table(MP_algorithm(B), B)
+  
+  if complete_search:
+    ans = [] 
+  state = -1
+  for i in range(len(A)):
+    while state >= 0 and A[i] != B[state + 1]:
+      state = RTB[state]
+    if A[i] != B[state + 1]:
+      continue
+      
+    state += 1
+    if state == len(B) - 1:
+      if complete_search:
+        ans.append(i - len(B) + 1)
+        state = RTB[state]
+      else:
+        return i - len(B) + 1
+    
+  return ans
