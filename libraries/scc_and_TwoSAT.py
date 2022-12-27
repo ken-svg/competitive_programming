aebe607d41ef119d8b9a651d27353ffb59cd5136
ken-svg <76655271+ken-svg@users.noreply.github.com>
2022-12-27 16:47:34 +0900

    Update scc_and_TwoSAT.py

--- libraries/scc_and_TwoSAT.py
+++ libraries/scc_and_TwoSAT.py
@@ -79,53 +79,93 @@ def topological_sort(I):
         vis[q] = True
   return ans_order, (len(ans_order) == N) # DAGでない場合、二つ目の戻り値がFalseとなる
 
+
+# 22/12/25 sccとは独立に実装します
 class Two_SAT():
   def __init__(self):
-    self.n = 0
     self.I = []
-    self.info = []
     
   def add(self, a, b, ta, tb): # (a != ta) -> (b == tb), (b != tb) -> (a == ta)
     I = self.I
     for _ in range((max(a, b) + 1) * 2 - len(self.I)):
       I.append([])   
-    I[(b<<1) + (1 ^ int(tb))]
-    I[(a<<1) + (1 ^ int(ta))].append((b<<1) + int(tb))    
-    I[(b<<1) + (1 ^ int(tb))].append((a<<1) + int(ta))
-    if a > b or (a == b and int(ta) > int(tb)):
-      a, b = b, a
-      ta, tb = tb, ta
+    I[(a<<1) | (1 ^ int(ta))].append((b<<1) | int(tb))    
+    I[(b<<1) | (1 ^ int(tb))].append((a<<1) | int(ta))
+  
+  def infer(self, a, b, ta, tb):  # (a == ta) -> (b == tb), (b == tb) -> (a == ta)
+    I = self.I
+    for _ in range((max(a, b) + 1) * 2 - len(self.I)):
+      I.append([])   
+    I[(a<<1) | int(ta)].append((b<<1) | int(tb))    
+    I[(b<<1) | (1 ^ int(tb))].append((a<<1) | (1 ^ int(ta)))
+  
+  def derive(self, constraint = None):
+    # constraintの与え方: [[Trueノード], [Falseノード]]
+    I = self.I
+    # 0) 拘束条件の処理
+    if constraint == None:
+      constraint = [[], []]
+    true = len(I) | 1　 # constraintの処理のため、必ずTrueとなる頂点を追加する
+    false = true ^ 1　
+    I.append([]) # I[false]
+    I.append([]) # I[true]
+    for c in constraint[0]: # Trueノード
+      #self.infer(true >> 1, c, True, True)
+      I[true].append((c << 1) | 1)
+      I[(c << 1)].append(false)
+    for c in constraint[1]: # Falseノード
+      #self.infer(true >> 1, c, True, False)
+      I[true].append((c << 1))
+      I[(c << 1) | 1].append(false)
+    for i in range(false): # trueが最下位、falseが最上位に来るための処理
+      #self.infer(i >> 1, true >> 1, i & 1, True)
+      I[i].append(true)
+      I[false].append(i ^ 1)
       
-    self.info.append([(a, ta), (b, tb)])
-    self.n = len(I) // 2
-  
-  def is_possible(self): # 充足可能かどうかを返す
-    I = self.I
-    C, C_inv, Ir = strongly_connected_component(I, 2 * self.n)
-    for i in range(self.n):
-      if C_inv[i << 1] == C_inv[(i << 1) | 1]:
-        return False  
-    return True
-    
-  def derive(self): # 充足可能なら解の一つを返し、そうでにならNoneを返す
-    I = self.I
-    C, C_inv, Ir = strongly_connected_component(I, 2 * self.n)
-    for i in range(self.n):
-      if C_inv[i << 1] == C_inv[(i << 1) | 1]:
-        return None  
-    top_order_Ir = topological_sort(Ir)[0]
-    TF = [-1] * self.n
-    for comp_id in top_order_Ir:
-      a0 = list(C[comp_id])[0]
-      if TF[a0 >> 1] == -1:
-        for a in C[comp_id]:
-          TF[a >> 1] = (a & 1) ^ 1
-          
-    return [bool(tf) for tf in TF]
-  
-  def __str__(self):
-    rtn = "<Two SAT on X_0 ~ X_{}> \n".format((self.n) - 1)
-    self.info.sort()
-    for (a, ta), (b, tb) in self.info:
-      rtn += (" X_{}".format(a) if ta else "-X_{}".format(a)) + " ∨ " + (" X_{}".format(b) if tb else "-X_{}".format(b)) + "\n"
-    return rtn[:-1] 
+    # 1) 帰りがけ順をDFSにより決定
+    N = len(I)
+    order = []
+    index_max = [len(i) for i in I]
+    path = [false]
+    index = [0] * N
+    vis = [False] * N
+    vis[false] = True
+    while path:
+      p = path[-1]
+      if index[p] == index_max[p]:
+        order.append(path.pop())
+        continue
+      q = I[p][index[p]]
+      index[p] += 1
+      if vis[q]: continue
+      vis[q] = True
+      path.append(q)
+      
+    #　2) 上流から順にTFを決定
+    TF = [None] * N
+    R = [[] for _ in range(N)]
+    for i in range(N):
+      for j in I[i]:
+        R[j].append(i)
+    for i in order[::-1]:
+      if not vis[i]: continue
+      vis[i] = False
+      task = [i]
+      comp = [i]
+      while task:
+        p = task.pop()
+        for q in R[p]:
+          if not vis[q]: continue
+          vis[q] = False
+          task.append(q)
+          comp.append(q)
+      # TFを確定
+      if TF[i^1] == None: # 相方が未確定
+        state = False
+      else: # 相方が確定(Falseのはず)
+        state = True
+      for p in comp: # TFを確定させながら、相方との矛盾が生じないか判定
+        if TF[p^1] != None and TF[p^1] == state: 
+          return None # 充足不能
+        TF[p] = state
+    return TF[1:len(TF)-1:2]
