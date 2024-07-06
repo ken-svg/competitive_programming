c7fbadf5d3b88d57122c3f72d7b4d0a9be94e883
ken-svg <76655271+ken-svg@users.noreply.github.com>
2024-07-06 18:22:59 +0900

    Update numbers.cpp

--- libraries_cpp/numbers.cpp
+++ libraries_cpp/numbers.cpp
@@ -1,21 +1,10 @@
-// mod 冪乗
-template <typename T>
-T pow(T x, T n, const T mod) {
-  T ret = 1;
-  while (n > 0) {
-    if (n & 1) {
-      (ret *= x) %= p;
-    }
-    (x *= x) %= p;
-    n >>= 1;
-  }
-  return ret;
-}
+#include <bits/stdc++.h>
+using namespace std;
 
 // ユークリッド互助法
 // 戻り値dは最小公約数。 参照変数x, yには、ax + by = dの解が入る
 template <typename T>
-T ext_gcd(T a, T b, T &x, T &y) {
+T ext_gcd(T a, T b, T &x, T &y){
   if (b == 0) {
     x = 1;
     y = 0;
@@ -26,15 +15,22 @@ T ext_gcd(T a, T b, T &x, T &y) {
   return d;
 }
 
-// modでの階乗 (ext_gcdを引くこと！！)
+// mod(素数)逆元
+template <typename T>
+T inv(T x, const T mod){
+  T x_inv, dummy;
+  ext_gcd(x, mod, x_inv, dummy);
+  return x_inv;
+}
+
+// modでの階乗 (ext_gcdを引用すること！！)
 template <typename T>
 void mod_factorial(vector<T> &F, vector<T> &F_inv, T N, const T mod){
   F = {1};
   F_inv = {1};
   for (T i = 1; i < N; i++){
     F.push_back((F.back() * i) % mod);
-    T i_inv, dum;
-    ext_gcd(i, mod, i_inv, dum);
+    T i_inv = inv(i, mod);
     F_inv.push_back((F_inv.back() * i_inv) % mod);
   }
   return ;
