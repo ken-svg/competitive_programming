1723d800cfed0b3055ae76b9b458356b0c107c2b
ken-svg <76655271+ken-svg@users.noreply.github.com>
2025-10-12 19:05:58 +0900

    Update LazySegTree_2(medium).py

--- libraries/LazySegTree_2(medium).py
+++ libraries/LazySegTree_2(medium).py
@@ -111,3 +111,93 @@ class LazySegTree():
       return op(op(ans_lt, data[l]), ans_rt)
     elif r == l:
       return op(ans_lt, ans_rt)
+      
+  def bisect(self, cond, l = 0): 
+    # 条件condを指定すると、cond(apply(l, r))がTrueとなる最大のrを返す。
+    # condは以下を満たす:
+    # ・cond(apply(l, r))がrについて単調　（例：op = min, cond(x) = [xはある数以上である] ）
+    # ・cond(apply(l, l)) = cond(self.ie) = True
+    
+    v = self.apply(l, self.size)
+    #print(v)
+    if cond(v): return self.n 
+    # はじめに、self.apply(l, self.size)を計算。この結果がTrueなら全区間[l, self.n)が答えなのでself.nを返す。
+    # この計算により、lの直上のノードにおけるlazyはすべてクリアされる。
+    
+    size = self.size; op = self.op; data = self.data; _push = self._push;
+    now_val = self.ie
+    now = l + size
+    while now & 1 == 0:
+      now >>= 1
+    # 左端をlとする最大区間を表すノードから開始。
+    
+    while True:
+      if cond(op(now_val, data[now])): # 現在の区間の分だけ延伸してもcondがTrueのまま
+        now_val = op(now_val, data[now])
+        now = now + 1
+        while now & 1 == 0:
+          now >>= 1
+        # 右隣へ移動
+      else: # 現在の区間を延伸するとFalseとなる
+        if now >= size: # 葉ノードであれば、現区間の左端を答えて終了
+          return now - size
+        
+        # そうでなければ、lazyを子へ伝播して、左の子へ移る
+        _push(now)
+        now <<= 1
+        
+  def bisect_r(self, cond, r): 
+    # 条件condを指定すると、cond(apply(l, r))がTrueとなる最小のlを返す。
+    # condは以下を満たす:
+    # ・cond(apply(l, r))がlについて単調　（例：op = min, cond(x) = [xはある数以上である] ）
+    # ・cond(apply(r, r)) = cond(self.ie) = True
+    
+    v = self.apply(0, r)
+    #print(v)
+    if cond(v): return 0
+    # はじめに、self.apply(0, r)を計算。この結果がTrueなら全区間[0, r)が答えなので0を返す。
+    # この計算により、rの直上のノードにおけるlazyはすべてクリアされる。
+    
+    size = self.size; op = self.op; data = self.data; _push = self._push;
+    now_val = self.ie
+    if r == size:
+      now = 1
+    else:
+      now = r + size
+      while now & 1 == 0:
+        now >>= 1
+      now -= 1
+    # 右端をrとする最大区間を表すノードから開始。
+    
+    while True:
+      if cond(op(data[now], now_val)): # 現在の区間の分だけ延伸してもcondがTrueのまま
+        now_val = op(data[now], now_val)
+        while now & 1 == 0:
+          now >>= 1
+        now -= 1
+        # 左隣へ移動
+      else: # 現在の区間を延伸するとFalseとなる
+        if now >= size: # 葉ノードであれば、現区間の右端を答えて終了
+          return now - size + 1
+        
+        # そうでなければ、lazyを子へ伝播して、右の子へ移る
+        _push(now)
+        now <<= 1
+        now += 1
+      
+  def __str__(self):
+    ans = ["LazySegmentTree : "]
+    def rec(l, r, p, d):
+      #print(p, self.size, l, r, self.data[p])
+      if r - l > 1:
+        c = (r + l) // 2
+        rec(l, c, p * 2, d + 1)
+      if p < self.size and self.lazy[p] != self.ic:
+        ans.append("_" + "______" * d + "[{:}, {:}) : {:} (act:{:})".format(l, r, self.data[p], self.lazy[p]))
+      else:
+        ans.append("_" + "______" * d + "[{:}, {:}) : {:}".format(l, r, self.data[p]))
+      if r - l > 1:
+        c = (r + l) // 2
+        rec(c, r, p * 2 + 1, d + 1)
+    rec(0, self.size, 1, 0)    
+    return "\n".join(ans)
